c8beab401e12c5166c5a3a5d103c3d0f09e7e8da
mborn319@gmail.com <mborn@brockettcreative.com>
2015-10-14 10:18:02 -0400

    Setup Jasmine test suite

---
+++ tests/main.js
@@ -0,0 +1,17 @@
+describe("social-feed.js is able to get a correct response from the social networks", function() {
+  it("returns status 200 when Facebook is called", function() {
+    expect(true).toBe(true);
+  });
+  it("returns status 200 when Twitter is called", function() {
+    expect(true).toBe(true);
+  });
+  it("returns status 200 when Google Plus is called", function() {
+    expect(true).toBe(true);
+  });
+  it("returns status 200 when Instagram is called", function() {
+    expect(true).toBe(true);
+  });
+  it("returns status 200 when VK is called", function() {
+    expect(true).toBe(true);
+  });
+});
