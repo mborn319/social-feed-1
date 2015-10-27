6885859a7e3c89e2a5a6913e5481d26278a95ad0
mborn319@gmail.com <mborn@brockettcreative.com>
2015-10-27 19:45:15 -0400

    Making headway in Jasmine-Ajax testing

--- tests/main.js
+++ tests/main.js
@@ -18,60 +18,66 @@ describe("option testing", function() {
         $('.socialFeed').html('').off();
     });
     it("gets correct post template.html", function() {
-            //activate Facebook
-            $('.socialFeed').socialfeed({
-                // FACEBOOK
-                facebook: {
-                    accounts: ["@ford","#ford"],
-                    limit: 2,
-                    access_token: "150849908413827|a20e87978f1ac491a0c4a721c961b68c"
-                },
-                template: "myTemplate.html"
-            });
+        //activate Facebook
+        $('.socialFeed').socialfeed({
+            // FACEBOOK
+            facebook: {
+                accounts: ["@ford"],
+                limit: 2,
+                access_token: "150849908413827|a20e87978f1ac491a0c4a721c961b68c"
+            },
+            template: "myTemplate.html"
+        });
 
             //it should be trying to get the post template
             expect(jasmine.Ajax.requests.first().url).toEqual("myTemplate.html");
     });
     it("doesn't call template.html if template_html parameter is provided", function() {
-            //activate Facebook
-            $('.socialFeed').socialfeed({
-                // FACEBOOK
-                facebook: {
-                    accounts: ["@ford","#ford"],
-                    limit: 2,
-                    access_token: "150849908413827|a20e87978f1ac491a0c4a721c961b68c"
-                },
-                template_html: '<div class="post">{{=it.text}}</div>'
-            });
-
+        var spy = spyOn($,"ajax");
+        spy.and.callFake(function(ajaxOpts) {
             //It should NOT be trying to get the template!
-            expect(jasmine.Ajax.requests.first().url).not.toBe("template.html");
+            expect(ajaxOpts.url).not.toBe("template.html");
+        });
+        //activate Facebook
+        $('.socialFeed').socialfeed({
+            // FACEBOOK
+            facebook: {
+                accounts: ["@ford"],
+                limit: 2,
+                access_token: "150849908413827|a20e87978f1ac491a0c4a721c961b68c"
+            },
+            template_html: '<div class="post">{{=it.text}}</div>'
+        });
+
     });
 
     it("doesn't show images or videos if show_media==false", function() {
-        config = {
-            clientid: "88b4730e0e2c4b2f8a09a6184af2e218"
-        };
-        //first we define the "ajax" response. We want to test with images.
-        jasmine.Ajax.stubRequest("https://api.instagram.com/v1/users/search/?q=ford&client_id="+config.clientid+"&count=1&callback=jQuery211036345327717325593_1445058650506&_=1445058650507").andReturn({
-            "responseText":"/**/ jQuery211036345327717325593_1445058650506({\"meta\":{\"code\":200},\"data\":[{\"username\":\"ford\",\"profile_picture\":\"https:\/\/scontent.cdninstagram.com\/hphotos-xfa1\/t51.2885-19\/11376530_1647567845473963_321960853_a.jpg\",\"id\":\"215007312\",\"full_name\":\"Ford Motor Company\"}]})"
-        });
-
+        var spy = spyOn($,"ajax");
+        spy.and.callFake(function(ajaxOpts) {
+            //this func is called when a request is submitted.
+            console.log("request made!",ajaxOpts)
+            console.log(jasmine.Ajax.requests.mostRecent())
+            //run the success callback with Ajax data containing images.
+            ajaxOpts.success({
+                data:{"created_time":"1445621846","link":"https://instagram.com/p/9MFF3JpCSZ/","images":{"low_resolution":{"url":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e35/12093730_1507891029525919_728814339_n.jpg","width":320,"height":320},"thumbnail":{"url":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s150x150/e35/c140.0.799.799/12081091_1639959399608146_2093722151_n.jpg","width":150,"height":150},"standard_resolution":{"url":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s640x640/sh0.08/e35/12093730_1507891029525919_728814339_n.jpg","width":640,"height":640}},"caption":{"created_time":"1445621846","text":"And here’s the third #FORDprofile submission that stopped us in our hashtag-searching tracks. Gotta love the moody cityscape caught by @mcalpinephotography. Ian drove high atop a parking garage in London, Ontario to get this shot—where is the coolest location you’ve found to capture your Ford?\n\nThanks to everyone who took the time to show off their rides.\n\n#Ford #Focus #FordFocus #city #photography","from":{"username":"ford","profile_picture":"https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-19/11376530_1647567845473963_321960853_a.jpg","id":"215007312","full_name":"Ford Motor Company"},"id":"1102278405050868998"},"type":"image","id":"1102278401846420633_215007312","user":{"username":"ford","profile_picture":"https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-19/11376530_1647567845473963_321960853_a.jpg","id":"215007312","full_name":"Ford Motor Company"}}
+            });
+        })//.and.callThrough();//let the call continue.
 
         //activate Facebook. Note that we do not Spy on the $.ajax function - we let it run its course, THEN examine the data.
         $('.socialFeed').socialfeed({
             // INSTAGRAM
             instagram: {
-                accounts: ["@ford","#ford"],
-                limit: 2,
+                accounts: ["@ford"],
+                limit: 10,
                 client_id: "88b4730e0e2c4b2f8a09a6184af2e218"
             },
-            template_html: '<div class="post">{{=it.text}}</div>',
-            show_media: false
+            template_html: '<div class="post">{{=it.text}}{{=it.attachment}}</div>',
+            show_media: true
         });
-
         //there should be no images in .socialFeed div, even though we "returned" some from the fake ajax call.
         expect($('.socialFeed').find("img").length).toBe(0);
+                //clear div contents & event handlers
+            //    $('.socialFeed').html('').off();
 
     });
 });
@@ -89,9 +95,19 @@ describe("AJAX", function() {
         //clear div contents & event handlers
         $('.socialFeed').empty().off();
     });
-    fit("calls Facebook for the userid",function() {
+    it("calls Facebook for the userid",function() {
 
-        console.log("before Facebook...");
+        var spy = spyOn($,"ajax");
+        spy.and.callFake(function(ajaxOpts) {
+            //this callback is called when the $.ajax function is run.
+            //The options string contains the arguments / options object which was passed to $.ajax by our plugin.
+
+            //make sure the $.ajax request url queries the @ford facebook page.
+            expect(ajaxOpts.url).toContain('https://graph.facebook.com/ford');
+            //make sure the access token is passed correctly
+            expect(ajaxOpts.url).toContain('access_token=150849908413827|a20e87978f1ac491a0c4a721c961b68c');
+        });
+
         //activate socialFeed with only Facebook details
         $('.socialFeed').socialfeed({
             // FACEBOOK
@@ -102,35 +118,31 @@ describe("AJAX", function() {
             },
             template_html: '<div class="post">{{=it.text}}</div>'
         });
-        console.log("after Facebook...");
-
-        //makes at least one request
-        expect(jasmine.Ajax.requests.count()).toBeGreaterThan(0);
-
-        //check that it's calling the correct url
-        var stripCallback = /[?]+[a-z0-9_=&]*/i
-        var lastAjaxUrl = jasmine.Ajax.requests.mostRecent().url.replace(stripCallback,'');
-        console.log("lastAjaxUrl=",lastAjaxUrl);
-        console.log("jasmine.Ajax.requests.mostRecent()=",jasmine.Ajax.requests.mostRecent());
-        console.log("jasmine.Ajax.requests.first()=",jasmine.Ajax.requests.first());
-
-        expect(jasmine.Ajax.requests.mostRecent().url).toBe('https://graph.facebook.com/ford/posts')
     });
-    /*it("calls Instagram for a list of posts",function() {
+    it("calls Instagram to get the users",function() {
+        var spy = spyOn($,"ajax");
+        spy.and.callFake(function(ajaxOpts) {
+            //check that it calls instagram, searching for the user @ford
+            expect( ajaxOpts.url).toContain("https://api.instagram.com/v1/users/search/?q=ford");
+            //check that it's correctly passing the client id
+            expect( ajaxOpts.url).toContain("client_id=88b4730e0e2c4b2f8a09a6184af2e218");
+            //check that it's asking for only one user
+            expect( ajaxOpts.url).toContain("count=1");
+        });
+
         //activate socialFeed with only Instagram details
         $('.socialFeed').socialfeed({
             // INSTAGRAM
             instagram: {
-                accounts: ["@ford","#ford"],
-                limit: 2,
+                accounts: ["@ford"],
                 client_id: "88b4730e0e2c4b2f8a09a6184af2e218"
             },
             // we use template_html for this test to skip the annoying call to template.html
             template_html: '<article class="twitter-post"><h4>{{=it.author_name}}</h4><p>{{=it.text}}<a href="{{=it.link}}" target="_blank">read more</a></p></article>'
         });
 
-        expect(jasmine.Ajax.requests.mostRecent().url).toEqual("https://api.instagram.com/v1/users/search/?q=ford")
-    });*/
+
+    });
 });
 
 /*
